1387638a4460a2bf335c45100e3c356f71420087
yeswanth65 <devisetty.yeswanth@gmail.com>
2020-07-15 11:43:49 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -1,12 +1,13 @@
 let b= Math.round(Math.random()*100);window.alert(b)+1;
       let count=0;
+      let round=10;
       document.getElementById("").innerHTML="pic.jpg"
     function printing(){
-             if(count<4){
+             if(count<round-1){
                     let a = document.getElementById("num").value;
                     if(a<=100 && a>=1){
                      if(a==b){
-                        document.getElementById("hint").innerHTML="Woah it is the number";
+                        document.getElementById("hint").innerHTML="You Guessed it right<br>Score:"+((round-count)*10);
                         
                      }
                       
@@ -27,8 +28,9 @@ let b= Math.round(Math.random()*100);window.alert(b)+1;
                  count++;
                   }
                   else{
-                      document.getElementById("hint").innerHTML="game over";     
+                      document.getElementById("hint").innerHTML="Game over <br> The number is "+(b+1); 
+                      document.getElementsByClassName("loss").innerHTML="lost.jpg"
                  }
-                  document.getElementById("chance").innerHTML=5-count;
+                  document.getElementById("chance").innerHTML=count;
                 }
                 
